2f0c65e99c32a342ec28a56c374b32905b2836e6
over-air <over_air@sohu.com>
2017-03-16 00:23:15 +0800

    test upload

--- vueWeb/src/App.vue
+++ vueWeb/src/App.vue
@@ -1,82 +1,32 @@
+
 <template>
   <div id="app">
     <img src="./assets/logo.png">
 
-    <el-upload
-      class="avatar-uploader"
-      action="http://localhost:8080/image/"
-      :show-file-list="false"
-      :on-success="handleAvatarScucess"
-      :before-upload="beforeAvatarUpload">
-      <img v-if="imageUrl" :src="imageUrl" class="avatar">
-      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-    </el-upload>
-
-
-
+    <Upload></Upload>
 
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data() {
-    return {
-      imageUrl: ''
-    };
-  },
-  methods: {
-    handleAvatarScucess(res, file) {
-      this.imageUrl = URL.createObjectURL(file.raw);
-    },
-    beforeAvatarUpload(file) {
-      const isJPG = file.type === 'image/jpeg';
-      const isLt2M = file.size / 1024 / 1024 < 2;
+  import Upload from './pages/upload.vue'
 
-      if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!');
-      }
-      if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!');
-      }
-      return isJPG && isLt2M;
+  export default {
+    name: 'app',
+    components:{
+      Upload
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
- .avatar-uploader .el-upload {
-   border: 1px dashed #d9d9d9;
-   border-radius: 6px;
-   cursor: pointer;
-   position: relative;
-   overflow: hidden;
- }
-.avatar-uploader .el-upload:hover {
-  border-color: #20a0ff;
-}
-.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 178px;
-  height: 178px;
-  line-height: 178px;
-  text-align: center;
-}
-.avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
